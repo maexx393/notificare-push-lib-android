1082980b3c237f8e7d68e9f92b760ccbd74f0b9b
silentjohnny <joris@notifica.re>
2013-09-20 13:11:14 +0200

    New version for SDK 1.0

--- Samples/PushTest/src/com/example/pushtest/MainActivity.java
+++ Samples/PushTest/src/com/example/pushtest/MainActivity.java
@@ -1,19 +1,24 @@
 package com.example.pushtest;
 
+import re.notifica.push.gcm.BaseActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
+import android.widget.Toast;
 
 /**
  * Main Activity class. Doesn't actually do anything
  * @author Joris Verbogt <joris@notifica.re>
  */
-public class MainActivity extends Activity {
+public class MainActivity extends BaseActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		if (getIntent().getAction().equals(Intent.ACTION_VIEW)) {
+			Toast.makeText(getApplication(), getIntent().getDataString(), Toast.LENGTH_LONG).show();
+		}
 	}
 
 	@Override
